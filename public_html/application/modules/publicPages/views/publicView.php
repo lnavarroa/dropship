304610b12d622e34e004a8d5bda3b1bba2041b7a
lnavarro <luis.navarro.arenas@gmail.com>
2019-06-02 15:22:02 -0400

    navbar 1era iteración

--- public_html/application/modules/publicPages/views/publicView.php
+++ public_html/application/modules/publicPages/views/publicView.php
@@ -3,11 +3,87 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="sp">
 <head>
-	<meta charset="utf-8">
-	<title>DropShip Chile</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>DropShip Chile</title>
+    <meta name="description" content="DropShip Chile &amp; un punto de encuentro entre bodegueros mayoristas y vendedores chilenos.">
+    <meta name="keywords" content="dropshipping chile, dropshipping, customers, retailers, dropshiper, mayoristas">
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:200, 300, 400,600,700, 900" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,300" rel="stylesheet" type="text/css">
+    <!-- Bootstrap -->
+    <link href="<?php base_url() ?>assets/bootstrap/dist/css/normalize.css" rel="stylesheet">
+    <link href="<?php base_url() ?>assets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php base_url() ?>assets/bootstrap/dist/css/styles.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 
 </head>
+
 <body>
+
+<!-- Just an image -->
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+  </a>
+</nav>
+
+<nav class="navbar navbar-dark bg-dark">
+  <!-- Navbar content -->
+</nav>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+        DropShip Chile</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="<?php base_url() ?>">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Que es el dropshipping</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Servicios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+  <!-- Navbar content -->
+</nav>
+
+<div class="top">
+        <div class="container-fluid">
+            <div class="cont-padding">
+                <div class="row">
+                    <div class="col-xs-6">
+                        DropShip Chile, portal de emprendedores mayoristas importadores</div>
+               
+				<div class="col-xs-6">
+					<div class="twitter"><img src="img/twitter.svg" alt="Twitter">Follow DropShipChile<a href="https://twitter.com/DropShipChile" rel="nofollow" target="_blank">@DropShipChile</a></div>
+					</div>     </div> </div>
+        </div>
+    </div>
+
+
 
 <div id="container">
 	<h1>Welcome to DropShip Chile.</h1>
@@ -31,5 +107,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
 </html>
+
+
